81db8c9b86ce649bbbf190ce49478008ec87ed98
Mahasak Pijittum <mahasak@gmail.com>
2018-07-09 23:42:00 -0700

    add initial macbook installer script

---
+++ setup_macbook.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+shell_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "$fmt\\n" "$@"
+}
+
+# Asking for macbook administrator password
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Welcome script
+shell_echo "Macbook Developer Setup Script"
+
+if ! command -v brew >/dev/null; then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
