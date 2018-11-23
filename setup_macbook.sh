bbccf979d333136af076da177f2b411caf367c91
Mahasak Pijittum <mahasak@gmail.com>
2018-11-23 23:00:49 +0800

    Update setup_macbook.sh

--- setup_macbook.sh
+++ setup_macbook.sh
@@ -71,29 +71,8 @@ brew install irssi geoip links nmap wget htop
 
 # Install CTF tools
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xz
+brew install aircrack-ng bfg binutils binwalk cifer dex2jar dns2tcp fcrackzip foremost hashpump hydra john knock netpbm nmap
+brew install pngcheck socat sqlmap tcpflow tcpreplay tcptrace ucspi-tcp xz
 
 brew install ack
 brew install dark-mode
